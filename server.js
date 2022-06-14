3f8827a937db8f91864a152c97ee160b08226364
Felipe Fernandes <f.fernandescs@gmail.com>
2022-06-14 06:52:20 -0300

    Configurando Node com netlify12

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const axios = require('axios')
 
 
 
@@ -8,20 +7,11 @@ const axios = require('axios')
 const port = process.env.PORT || 4568
 
 
-
-app.get('/', async(req, res) => {
-    const lojaProxima = req.query
-    const { data } = await axios(`https://mercado.carrefour.com.br/api/catalog_system/pub/products/search?fq=${lojaProxima}`)
-
-    return res.json(data)
+app.get('/', (req, res)  => {
+    res.send('Ola AMigos')
 })
 
-app.get('/home', async(req, res) => {
-    const cepStorage = req.query
-    const { data } = await axios(`https://mercado.carrefour.com.br/api/checkout/pub/regions?country=BRA&postalCode=${cepStorage.cep}`)
-    return res.json(data)
 
-})
 
 
 
