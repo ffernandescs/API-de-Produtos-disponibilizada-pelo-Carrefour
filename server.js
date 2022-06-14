b49b04e80e588c5f69d212450e6c95bd87adfe95
Felipe Fernandes <f.fernandescs@gmail.com>
2022-06-14 05:32:58 -0300

    Configurando Node com netlify4

--- server.js
+++ server.js
@@ -1,16 +1,59 @@
-const express = require("express");
-               const serverless = require("serverless-http");
-        
-               const app = express();
-               const router = express.Router();
- 
-                router.get("/", (req, res) => {  
-	            res.json({
-    		        hello: "hi!",
-                      });
-                 });
+const cors = require('cors')
+const express = require('express')
+const app = express()
+const axios = require('axios')
 
-                app.use(`/.netlify/functions/api`, router);
+const port = process.env.PORT || 3000;
 
-               module.exports = app;
-               module.exports.handler = serverless(app);
+app.use(cors())
+
+// Ira passar informação da loja para pagina inicial, a mesma ja recebe o valor do frontEnd
+
+app.get('/', async(req, res) => {
+    const lojaProxima = req.query
+    const { data } = await axios(`https://mercado.carrefour.com.br/api/catalog_system/pub/products/search?fq=${lojaProxima}`)
+
+    return res.json(data)
+})
+
+// Ira passar informação da Cep para pagina inicial, a mesma ja recebe o valor do frontEnd
+
+app.get('/home', async(req, res) => {
+    const cepStorage = req.query
+    const { data } = await axios(`https://mercado.carrefour.com.br/api/checkout/pub/regions?country=BRA&postalCode=${cepStorage.cep}`)
+    return res.json(data)
+
+})
+
+// Ira passar informação da loja para pagina Lojas, a mesma ja recebe o valor do frontEnd
+
+
+app.get('/lojas', async(req, res) => {
+    const lojaProxima = req.query
+    const { data } = await axios(`https://mercado.carrefour.com.br/api/catalog_system/pub/products/search?fq=${lojaProxima}`)
+    console.log(lojaProxima)
+
+    return res.json(data)
+})
+
+// Ira passar informação da Cep para pagina Lojas, a mesma ja recebe o valor do frontEnd
+
+
+app.get('/lojasCp', async(req, res) => {
+    const cep = req.query
+    const { data } = await axios(`https://mercado.carrefour.com.br/api/checkout/pub/regions?country=BRA&postalCode=${cep.cep}`)
+    return res.json(data)
+})
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.info('Aplicação Rodando')
+})
