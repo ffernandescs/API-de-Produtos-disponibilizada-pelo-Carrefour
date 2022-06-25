1fb8dd039c2592eaa76c538962f61b4325c592de
Felipe Fernandes <83374517+ffernandescs@users.noreply.github.com>
2022-06-25 13:27:26 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,50 @@
+const cors = require('cors')
 const express = require('express')
 const app = express()
+const axios = require('axios')
 
+app.use(cors())
 
+const PORT = process.env.PORT || 4567 
 
+// Ira passar informação da loja para pagina inicial, a mesma ja recebe o valor do frontEnd
 
-const port = process.env.PORT || 4568
+app.get('/', async(req, res) => {
+    const lojaProxima = req.query
+    const { data } = await axios(`https://mercado.carrefour.com.br/api/catalog_system/pub/products/search?fq=${lojaProxima}`)
 
-
-app.get('/', async (req, res)  => {
-    res.send('Ola AMigosBetch')
+    return res.json(data)
 })
 
+// Ira passar informação da Cep para pagina inicial, a mesma ja recebe o valor do frontEnd
 
-
-
-
-
-app.listen(port, () => {
-    console.info('Aplicação Rodando')
+app.get('/home', async(req, res) => {
+    const cepStorage = req.query
+    const { data } = await axios(`https://mercado.carrefour.com.br/api/checkout/pub/regions?country=BRA&postalCode=${cepStorage.cep}`)
+    return res.json(data)
 })
 
+// Ira passar informação da loja para pagina Lojas, a mesma ja recebe o valor do frontEnd
+
+
+app.get('/lojas', async(req, res) => {
+    const lojaProxima = req.query
+    const { data } = await axios(`https://mercado.carrefour.com.br/api/catalog_system/pub/products/search?fq=${lojaProxima}`)
+    console.log(lojaProxima)
+
+    return res.json(data)
+})
+
+// Ira passar informação da Cep para pagina Lojas, a mesma ja recebe o valor do frontEnd
+
+
+app.get('/lojasCp', async(req, res) => {
+    const cep = req.query
+    const { data } = await axios(`https://mercado.carrefour.com.br/api/checkout/pub/regions?country=BRA&postalCode=${cep.cep}`)
+    return res.json(data)
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`)
+})
 
